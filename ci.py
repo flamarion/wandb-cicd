05008c47e3e5a92c3d460d464f34cc567f545aa5
Flamarion Jorge <flamarion.jorge@wandb.com>
2024-05-30 14:36:09 +0200

    pr for test

--- ci.py
+++ ci.py
@@ -2,5 +2,5 @@ import wandb
 import os
 
 print(f'This is the version of wandb: {wandb.__version__}')
-assert wandb.__version__ == '0.10.33', f'Expected version 0.10.33, but got {wandb.__version__}'
+assert wandb.__version__ == '0.17.0', f'Expected version 0.17.0, but got {wandb.__version__}'
 # wandb.login()
